e9c02924caa4e3d97627151d85d5d49a9ecd3d9b
Arthur Emidio <arthur.500@gmail.com>
2016-05-04 23:53:49 -0300

    Initial Commit

---
+++ AudioPlayer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AudioPlayer
+//
+//  Created by Arthur Emídio Teixeira Ferreira on 5/4/16.
+//  Copyright © 2016 Arthur Emidio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
